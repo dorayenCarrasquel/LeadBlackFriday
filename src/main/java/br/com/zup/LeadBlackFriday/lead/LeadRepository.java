e0e3d4420674b852ea8b5cf71e7f6c19c58b9002
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-18 11:11:13 -0300

    Adicionar Repositorios

---
+++ src/main/java/br/com/zup/LeadBlackFriday/lead/LeadRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.LeadBlackFriday.lead;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LeadRepository extends CrudRepository {
+
+}
---
+++ src/main/java/br/com/zup/LeadBlackFriday/producto/ProductoRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.LeadBlackFriday.producto;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductoRepository extends CrudRepository {
+}
