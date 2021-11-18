19eaeaf85efbbe161f1a32b0d73d247764f16bf7
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-18 11:17:34 -0300

    Fix Assinatura do CrudREpository

--- src/main/java/br/com/zup/LeadBlackFriday/lead/LeadRepository.java
+++ src/main/java/br/com/zup/LeadBlackFriday/lead/LeadRepository.java
@@ -2,6 +2,6 @@ package br.com.zup.LeadBlackFriday.lead;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface LeadRepository extends CrudRepository {
+public interface LeadRepository extends CrudRepository<Lead,String> {
 
 }
--- src/main/java/br/com/zup/LeadBlackFriday/producto/ProductoRepository.java
+++ src/main/java/br/com/zup/LeadBlackFriday/producto/ProductoRepository.java
@@ -2,5 +2,5 @@ package br.com.zup.LeadBlackFriday.producto;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface ProductoRepository extends CrudRepository {
+public interface ProductoRepository extends CrudRepository<Producto,Integer> {
 }
