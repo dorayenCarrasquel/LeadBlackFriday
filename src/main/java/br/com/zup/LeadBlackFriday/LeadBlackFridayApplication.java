4ca6f3c061a08f4fc3e0415fa873d611bd562552
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-11-18 10:26:45 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/zup/LeadBlackFriday/LeadBlackFridayApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.LeadBlackFriday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadBlackFridayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadBlackFridayApplication.class, args);
+	}
+
+}
